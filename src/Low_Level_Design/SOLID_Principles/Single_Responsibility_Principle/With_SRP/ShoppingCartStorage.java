c40d214f2e8b3f8117337d2a3899396cc8bdea13
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-07-09 13:09:15 +0530

    add code logic

--- src/Low_Level_Design/SOLID_Principles/Single_Responsibility_Principle/With_SRP/ShoppingCartStorage.java
+++ src/Low_Level_Design/SOLID_Principles/Single_Responsibility_Principle/With_SRP/ShoppingCartStorage.java
@@ -1,4 +1,14 @@
 package Low_Level_Design.SOLID_Principles.Single_Responsibility_Principle.With_SRP;
 
 public class ShoppingCartStorage {
+
+    private final ShoppingCart cart;
+
+    public ShoppingCartStorage(ShoppingCart cart){
+        this.cart = cart;
+    }
+
+    public String saveToDataBase(){
+        return "saving shopping cart to database...";
+    }
 }
