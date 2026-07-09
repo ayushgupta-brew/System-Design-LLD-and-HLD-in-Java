700805ad5dd5bdb863cae606c514738003deffde
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-07-09 12:52:11 +0530

    add code logic

--- src/Low_Level_Design/SOLID_Principles/Single_Responsibility_Principle/Without_SRP/Product.java
+++ src/Low_Level_Design/SOLID_Principles/Single_Responsibility_Principle/Without_SRP/Product.java
@@ -1,4 +1,11 @@
 package Low_Level_Design.SOLID_Principles.Single_Responsibility_Principle.Without_SRP;
 
 public class Product {
+    public String name;
+    public double price;
+
+    public Product(String name , double price){
+        this.name = name;
+        this.price = price;
+    }
 }
