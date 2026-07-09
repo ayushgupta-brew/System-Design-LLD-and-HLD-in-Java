f7e2cb952df26ad718736c9cff2992024caf42c2
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-07-09 13:09:01 +0530

    add code logic

--- src/Low_Level_Design/SOLID_Principles/Single_Responsibility_Principle/With_SRP/ShoppingCartPrinter.java
+++ src/Low_Level_Design/SOLID_Principles/Single_Responsibility_Principle/With_SRP/ShoppingCartPrinter.java
@@ -1,4 +1,17 @@
 package Low_Level_Design.SOLID_Principles.Single_Responsibility_Principle.With_SRP;
 
 public class ShoppingCartPrinter {
+    private final ShoppingCart cart;
+
+    public ShoppingCartPrinter(ShoppingCart cart){
+        this.cart = cart;
+    }
+
+    public void printInVoice(){
+        System.out.println("Shopping Cart Invoice:");
+        for (Product product : cart.getProducts()) {
+            System.out.println(product.name + " - Rs " + product.price);
+        }
+        System.out.println("Total: Rs " + cart.calculateTotal());
+    }
 }
