9793667e292efc7c1d8283a2ee577b128303c9cc
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-07-09 12:51:49 +0530

    add code logic

--- src/Low_Level_Design/SOLID_Principles/Single_Responsibility_Principle/Without_SRP/Main.java
+++ src/Low_Level_Design/SOLID_Principles/Single_Responsibility_Principle/Without_SRP/Main.java
@@ -1,4 +1,15 @@
 package Low_Level_Design.SOLID_Principles.Single_Responsibility_Principle.Without_SRP;
 
 public class Main {
+    static void main() {
+        ShoppingCart shoppingCart = new ShoppingCart();
+
+        shoppingCart.addProduct(new Product("Laptop", 50000));
+        shoppingCart.addProduct(new Product("Mouse", 2000));
+
+        shoppingCart.printInvoice();
+        String messageFromDB = shoppingCart.saveToDatabase();
+
+        System.out.println(messageFromDB);
+    }
 }
