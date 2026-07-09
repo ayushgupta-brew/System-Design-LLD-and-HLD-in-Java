12007b3146fc64d769afbe71e8eafbd7ff2154b3
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-07-09 13:08:50 +0530

    add code logic

--- src/Low_Level_Design/SOLID_Principles/Single_Responsibility_Principle/With_SRP/ShoppingCart.java
+++ src/Low_Level_Design/SOLID_Principles/Single_Responsibility_Principle/With_SRP/ShoppingCart.java
@@ -1,4 +1,23 @@
 package Low_Level_Design.SOLID_Principles.Single_Responsibility_Principle.With_SRP;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
+
+    private final List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product product){
+        products.add(product);
+    }
+    public List<Product> getProducts(){
+        return products;
+    }
+    public double calculateTotal(){
+        double total = 0;
+        for(Product product : products) {
+            total += product.price;
+        }
+        return total;
+    }
 }
