e90cdaa7449fcb2bb66dab1611bb7b5e2f5a658c
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-07-12 12:41:11 +0530

    add core logic

--- src/Low_Level_Design/SOLID_Principles/Open_Closed_Principles/Without_OCP/Product.java
+++ src/Low_Level_Design/SOLID_Principles/Open_Closed_Principles/Without_OCP/Product.java
@@ -1,4 +1,11 @@
 package Low_Level_Design.SOLID_Principles.Open_Closed_Principles.Without_OCP;
 
 public class Product {
+    private String name;
+    private double price;
+
+    Product(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
 }
