e80647e1b920aef8458170b81c428cb057bc86be
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-07-12 12:48:56 +0530

    add core logic

--- src/Low_Level_Design/SOLID_Principles/Open_Closed_Principles/Without_OCP/ShoppingCart.java
+++ src/Low_Level_Design/SOLID_Principles/Open_Closed_Principles/Without_OCP/ShoppingCart.java
@@ -1,4 +1,24 @@
 package Low_Level_Design.SOLID_Principles.Open_Closed_Principles.Without_OCP;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShoppingCart {
+
+    private final List<Product> products = new ArrayList<>();
+    void addProduct(Product p) {
+        products.add(p);
+    }
+
+    List<Product> getProducts() {
+        return products;
+    }
+
+    double calculateTotal() {
+        double total = 0;
+        for (Product p : products) {
+            total += p.getPrice();
+        }
+        return total;
+    }
 }
