be2b3bd74ad1d8a22a0872a71c370cdb3cd489ed
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-07-12 12:55:10 +0530

    add core logic

--- src/Low_Level_Design/SOLID_Principles/Open_Closed_Principles/Without_OCP/Main.java
+++ src/Low_Level_Design/SOLID_Principles/Open_Closed_Principles/Without_OCP/Main.java
@@ -1,4 +1,13 @@
 package Low_Level_Design.SOLID_Principles.Open_Closed_Principles.Without_OCP;
 
 public class Main {
+    static void main() {
+        ShoppingCart cart = new ShoppingCart();
+
+        cart.addProduct(new Product("Laptop", 50000));
+        cart.addProduct(new Product("Mouse", 2000));
+
+        ShoppingCartStorage storeToDB = new ShoppingCartStorage(cart);
+        storeToDB.saveToSQLDatabase();
+    }
 }
--- src/Low_Level_Design/SOLID_Principles/Open_Closed_Principles/Without_OCP/ShoppingCartStorage.java
+++ src/Low_Level_Design/SOLID_Principles/Open_Closed_Principles/Without_OCP/ShoppingCartStorage.java
@@ -1,4 +1,21 @@
 package Low_Level_Design.SOLID_Principles.Open_Closed_Principles.Without_OCP;
 
 public class ShoppingCartStorage {
+    private final ShoppingCart cart;
+
+    public ShoppingCartStorage(ShoppingCart cart) {
+        this.cart = cart;
+    }
+
+    void saveToSQLDatabase() {
+        System.out.println("Saving shopping cart to SQL DB...");
+    }
+
+    void saveToMongoDatabase() {
+        System.out.println("Saving shopping cart to Mongo DB...");
+    }
+
+    void saveToFile() {
+        System.out.println("Saving shopping cart to File...");
+    }
 }
