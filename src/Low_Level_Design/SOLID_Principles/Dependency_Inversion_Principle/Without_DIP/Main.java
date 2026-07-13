450e776d50ba9b81021aa55ea91635f106d51175
Brew-and-Bugs-with-Ayush <nishaayushgupta22@gmail.com>
2026-07-13 13:03:08 +0530

    add core logic

--- src/Low_Level_Design/SOLID_Principles/Dependency_Inversion_Principle/Without_DIP/Main.java
+++ src/Low_Level_Design/SOLID_Principles/Dependency_Inversion_Principle/Without_DIP/Main.java
@@ -1,4 +1,10 @@
 package Low_Level_Design.SOLID_Principles.Dependency_Inversion_Principle.Without_DIP;
 
 public class Main {
+    static void main() {
+        UserService userService = new UserService();
+
+        userService.storeUserToSQL("Ayush");
+        userService.storeUserToMongo("Nisha");
+    }
 }
